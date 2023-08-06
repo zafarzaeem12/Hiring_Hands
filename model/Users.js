a3069c529b5de31828dc1523a9fc9641dd6fad3f
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2023-08-06 23:36:41 +0500

    ready to start

---
+++ model/Users.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+        // unique: true,
+        // required: true
+    },
+    user_image: {
+        type: String,
+    },
+    phone_number: {
+        type: String,
+        // required: true
+    },
+    dob: {
+        type: String,
+       // required: true
+    },
+    address: {
+        type: String,
+    },
+    gender:{
+        type:String,
+        enum : ['Male','Female' , 'Prefer Not to Say'],
+        default: 'Male'
+    },
+    role:{
+        type:String,
+        enum : ['Admin','User'],
+        default: 'User'
+    },
+    state: {
+        type: String,
+    },
+    city: {
+        type: String,
+    },
+    verification_code :{
+        type:Number,
+        default : 0
+    },
+    is_verified :{
+        type: Boolean,
+        default : false
+    },
+    user_is_profile_complete :{
+        type: Boolean,
+        default : false
+    },
+    user_is_forgot :{
+        type: Boolean,
+        default : false
+    },
+    user_authentication: {
+        type: String,
+        default : ""
+    },
+    user_social_token: {
+        type: String,
+    },
+    user_social_type: {
+        type: String,
+    },
+    user_device_token: {
+        type: String,
+        default : "",
+    },
+    user_device_type: {
+        type: String,
+        required : true
+        
+    },
+    is_profile_deleted :{
+        type: Boolean,
+        default : false
+    },
+    is_notification :{
+        type: Boolean,
+        default : false
+    },
+    is_Blocked :{
+        type: Boolean,
+        default : false
+    },
+    user_social_token:{
+        type : String,
+        default : '12345'
+    },
+    user_social_type:{
+        type:String,
+        enum : ['Phone','Google','Apple','none'],
+        default: 'none'
+    }
+},
+    { timestamps: true }
+)
+module.exports = mongoose.model("User", UserSchema);
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const databaseConnection = require('./database/databaseConnection')
+
+// app routes start here
+const UserRouter = require('./router/Users')
+// const PreferenceRouter = require('./router/Preferences')
+// const TypesRouter = require('./router/Types')
+// const LikedRouter = require('./router/Likeed')
+// const CommentsRouter = require('./router/Comments')
+// const ReportsRouter = require('./router/Report')
+// const GoalsRouter = require('./router/Goals')
+// const Save_Types = require('./router/Save_Types')
+// const NotificationRouter = require('./router/Notification');
+// app routes end here
+
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.json());
+app.use(cors());
+
+// app routes use here
+app.use("/UserAPI/", UserRouter);
+// app.use("/PreferenceAPI/", PreferenceRouter);
+// app.use("/TypesAPI/", TypesRouter);
+// app.use("/LikedAPI/", LikedRouter);
+// app.use("/CommentsAPI/", CommentsRouter);
+// app.use("/ReportAPI/", ReportsRouter);
+// app.use("/GoalAPI/", GoalsRouter);
+// app.use("/Save_TypesAPI/", Save_Types);
+// app.use("/NotificationAPI/", NotificationRouter);
+
+// routes end here
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+// database connection start here
+databaseConnection()
+// database connection end here
+
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port} Port`);
+});
